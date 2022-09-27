d833291bee84adddf99f6d2bda633cdfd2231512
Miguel Dela Cruz <mcdc171@gmail.com>
2022-09-27 19:09:11 +0800

    F2F activities

---
+++ exercise_f2f_activity/php_Exercise1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //echo without variable 
+    echo "Twinkle, Twinkle little star.</br>";
+    
+    //echo with 2 variable
+    $var1= "twinkle";
+    $var2= "star";
+
+    echo "$var1, $var1 little $var2!"
+    ?>
+</body>
+</html>
---
+++ exercise_f2f_activity/php_Exercise2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //variables
+    $x=10;
+    $y=7; 
+
+
+    //operators
+    echo "$x + $y=" .($x+$y)."<br>";
+    echo "$x - $y=" .($x-$y)."<br>";
+    echo "$x * $y=" .($x*$y)."<br>";
+    echo "$x / $y=" .($x/$y)."<br>";
+    echo "$x % $y=" .($x%$y)."<br>";
+    
+    ?>
+</body>
+</html>
